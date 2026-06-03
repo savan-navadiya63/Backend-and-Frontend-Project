3d4b098cf364d01b217f13d442ba861d2bc88f59
savan navadiya <snavadiya600@gmail.com>
2026-06-03 13:27:47 +0530

    Backend change

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();  // .env file se environment variables ko load krna
 
 const app = require('./src/app');
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(5000,() => {
-    console.log('Server is running on port 5000');   
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

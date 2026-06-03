9f8644f8d63492064806bef07db608f07d63d433
savan navadiya <snavadiya600@gmail.com>
2026-06-03 11:32:58 +0530

    Backend folder remove

--- Backend/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();  // .env file se environment variables ko load krna
-
-const app = require('./src/app');
-
-
-app.listen(5000,() => {
-    console.log('Server is running on port 5000');   
-});
